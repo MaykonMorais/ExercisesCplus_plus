a698c24b04f1eecaf5bb59c98330e61f7c893749
Maykon Morais <maykon.estudos@gmail.com>
2019-03-23 15:45:19 -0300

    Funções Com Strings

--- Funções Com Strings e Registros/Lab26-Ex02.cpp
+++ Funções Com Strings e Registros/Lab26-Ex02.cpp
@@ -1,26 +1,23 @@
-#include  <iostream>
+#include <iostream>
+#include <fstream>
 using namespace std;
 int subst(char * str, char c1, char c2);
-
 int main() {
-	char nome[40];
-	cout << "Digite seu nome: ";
-	cin.get(nome, 40);
+	char nome[10];
+	cout << "Digite teu nome: ";
+	cin >> nome;
+	int  tot = subst(nome, 'a', 'e');
 
-	char c1 = 'a';
-	char c2 = 'e';
-
-
-	cout << "O numero de substituicoes: " << subst(nome, c1, c2) << endl;
-	cout << "Nome Substituido: " << nome << endl;
+	cout << "Nome trocado: " << nome << endl;
+	cout << "Substituicoes: " << tot << endl;
 	system("pause");
 	return 0;
 }
+
 int subst(char * str, char c1, char c2) {
 	int cont = 0;
-	
 	while (*str) {
-		if (*str == c1) {
+		if (*str == c1){
 			*str = c2;
 			++cont;
 		}
@@ -28,4 +25,3 @@ int subst(char * str, char c1, char c2) {
 	}
 	return cont;
 }
-
